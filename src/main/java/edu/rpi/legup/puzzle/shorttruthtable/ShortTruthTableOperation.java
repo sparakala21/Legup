8aac3e0cd052a2a0a4d091bcdf4091fd20a11fe5
YoshikoJanai <39793714+YoshikoJanai@users.noreply.github.com>
2021-02-27 09:15:10 -0800

    Looking at https://github.com/Bram-Hub/Legup/commit/825ad9d4cde81b8ffac2cc4d9e98857a3a1efd97 which is where STT parsing most likely broke

--- src/main/java/edu/rpi/legup/puzzle/shorttruthtable/ShortTruthTableOperation.java
+++ src/main/java/edu/rpi/legup/puzzle/shorttruthtable/ShortTruthTableOperation.java
@@ -22,32 +22,42 @@ public class ShortTruthTableOperation{
 
     public static String getLogicSymbol(char c){
         switch(c){
-            case AND_1: return "\u2227";
-            case OR_1: return "\u2228";
-            case NOT_1: return "\u00AC";
-            case CONDITIONAL_1: return "\u2192";
-            case BICONDITIONAL_1: return "\u2194";
-            case AND_2: return "\u2227";
-            case OR_2: return "\u2228";
-            case NOT_2: return "\u00AC";
-            case CONDITIONAL_2: return "\u2192";
-            case BICONDITIONAL_2: return "\u2194";
+            case AND_1:
+            case AND_2:
+                return "\u2227";
+            case OR_1:
+            case OR_2:
+                return "\u2228";
+            case NOT_1:
+            case NOT_2:
+                return "\u00AC";
+            case CONDITIONAL_1:
+            case CONDITIONAL_2:
+                return "\u2192";
+            case BICONDITIONAL_1:
+            case BICONDITIONAL_2:
+                return "\u2194";
         }
         return "" + c;
     }
 
     public static String getRuleName(char operation){
         switch(operation){
-            case AND_1: return "And";
-            case OR_1: return "Or";
-            case NOT_1: return "Not";
-            case CONDITIONAL_1: return "Conditional";
-            case BICONDITIONAL_1: return "Biconditional";
-            case AND_2: return "And";
-            case OR_2: return "Or";
-            case NOT_2: return "Not";
-            case CONDITIONAL_2: return "Conditional";
-            case BICONDITIONAL_2: return "Biconditional";
+            case AND_1:
+            case AND_2:
+                return "And";
+            case OR_1:
+            case OR_2:
+                return "Or";
+            case NOT_1:
+            case NOT_2:
+                return "Not";
+            case CONDITIONAL_1:
+            case CONDITIONAL_2:
+                return "Conditional";
+            case BICONDITIONAL_1:
+            case BICONDITIONAL_2:
+                return "Biconditional";
         }
         return null;
     }
