0e0a427bd22ec814db6077d59b673b471d0de785
sravan parakala <sravanparakala@gmail.com>
2023-03-24 17:39:11 -0400

    Update image function
    i wrote an update function to change the image based on legupPreferences

--- src/main/java/edu/rpi/legup/model/rules/Rule.java
+++ src/main/java/edu/rpi/legup/model/rules/Rule.java
@@ -108,6 +108,30 @@ public abstract class Rule {
         }
     }
 
+    public void updateImage()
+    {
+        LegupPreferences prefs = LegupPreferences.getInstance();
+        if(imageName.contains("shorttruthtable") && prefs.getUserPref(LegupPreferences.COLOR_BLIND).equals("true")) {
+            imageName = imageName.replace("ruleimages", "ruleimages_cb");
+        }
+        this.image = new ImageIcon(ClassLoader.getSystemClassLoader().getResource(imageName));
+
+        //Resize images to be 100px wide
+        Image image = this.image.getImage();
+        if (this.image.getIconWidth() < 120) return;
+        int height = (int) (100 * ((double) this.image.getIconHeight() / this.image.getIconWidth()));
+        if (height == 0) {
+            System.out.println("height is 0 error");
+            System.out.println("height: " + this.image.getIconHeight());
+            System.out.println("width:  " + this.image.getIconWidth());
+            return;
+        }
+        BufferedImage bimage = new BufferedImage(100, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bimage.createGraphics();
+        g.drawImage(image, 0, 0, 100, height, null);
+        this.image = new ImageIcon(bimage);
+    }
+
     /**
      * Gets the name of the rule
      *
